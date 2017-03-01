48bdf9d5a4421e3318f9d144833f9d6d53e346a8
Alfonso Kim <alfonso.kimq@gmail.com>
2017-03-01 10:22:37 -0600

    varios

--- code/s5_conteo.py
+++ code/s5_conteo.py
@@ -2,6 +2,7 @@
 import urllib2
 from collections import defaultdict
 
+# =============================================================================
 def world_count(url):
     counts = defaultdict(int)
     for line in urllib2.urlopen(url).readlines():
@@ -10,13 +11,14 @@ def world_count(url):
             counts[world] += 1
     return counts
 
+# =============================================================================
 def to_csv(counts):
     csv_file = open('quixote_world_count.csv', 'w')
     for world, freq in counts.iteritems():
         print >> csv_file, '%s,%i' % (world, freq)
     csv_file.close()
 
-
+# =============================================================================
 if __name__ == '__main__':
     counts = world_count('http://www.gutenberg.org/cache/epub/996/pg996.txt')
     sorted_keys = sorted(counts, key=counts.get, reverse=True)
---
+++ code/s6_varios.py
@@ -0,0 +1,52 @@
+
+# =============================================================================
+class Magic():
+
+    def __init__(self, **props):
+        self.props = props
+
+    def __getattr__(self, attr):
+        if attr in self.props:
+            return self.props.get(attr)
+        raise Exception('No existe: %s' % attr)
+
+    def __call__(self, *args, **keywords):
+        print 'call con args: %s y keywords: %s' % (str(args), str(keywords))
+
+
+# =============================================================================
+m = Magic(**{'hello': 1, 'world': range(10)})
+
+print m.hello
+print m.world
+# print m.no_existe
+
+m()
+m(1, 'dos', False)
+m(1, dos=2, tres='tres')
+
+# =============================================================================
+def timer(f):
+    import time
+    def proxy(*args):
+        start = time.time()
+        val = f(*args)
+        print 'tiempo: %f' % (time.time() - start)
+        return val
+    return proxy
+
+import math
+
+@timer
+def expensive(test_f, n):
+    return [test_f(x) for i in range(n) for x in range(i)]
+
+test1 = lambda x: x**0.5
+test2 = lambda x: math.log(x+1, 3)
+test3 = lambda x: x / 2
+
+expensive(test1, 100)
+expensive(test2, 100)
+expensive(test3, 100)
+
+
