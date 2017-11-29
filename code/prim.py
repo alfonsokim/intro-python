3c6d3d8216d72666e960fcf6f819ec3c0a0e7a62
Alfonso Kim <alfonso.kimq@gmail.com>
2017-11-29 10:19:47 -0600

    revision...

--- code/prim.py
+++ code/prim.py
@@ -13,19 +13,18 @@ def prim(graph):
     free_edges.remove(root)
     free_nodes = set(''.join(n[0] for n in free_edges))
     while len(free_nodes) > 0:
-        # edges = [edge for edge in free_edges if edge[0][0] in used_nodes or edge[0][1] in used_nodes]
-        edges = [edge for edge in free_edges if edge[0][0] in used_nodes]
-        print edges
+        edges = [edge for edge in free_edges if edge[0][0] in used_nodes or edge[0][1] in used_nodes]
+        # edges = [edge for edge in free_edges if edge[0][0] in used_nodes]
         min_edge = min(edges, key=lambda e: e[1])
-        # free_nodes.discard(min_edge[0][0])
+        free_nodes.discard(min_edge[0][0])
         free_nodes.discard(min_edge[0][1])
         free_edges.remove(min_edge)
         min_spanning_tree.append(min_edge)
         used_nodes.update(min_edge[0])
         # print 'edges: %s' % str(edges)
         # print 'min_edge: %s' % str(min_edge)
-        print 'used_nodes: %s' % str(used_nodes)
-        print 'free_edges: %s' % str(free_edges)
+        # print 'used_nodes: %s' % str(used_nodes)
+        # print 'free_edges: %s' % str(free_edges)
         # print 'min_spanning_tree(%i): %s' % (len(min_spanning_tree), str(min_spanning_tree))
     return min_spanning_tree
 
